ab8bf98e5fcc3410f82620a3b122431d803af092
Torbet McNeil <tmcneil@arizona.edu>
2026-02-13 16:02:01 -0700

    Add files via upload

---
+++ mixed-models/ordinal-data-management.2.R
@@ -0,0 +1,18 @@
+install.packages("ordinal")
+library(ordinal)
+install.packages("emmeans")
+library(emmeans)
+
+# Read data
+scores_long <- read.csv("C:/Users/ual-laptop/Desktop/data_management_long.csv")
+# Ordinal package is expecting a factor for the response variable
+scores_long$ScoreOrdinal <- as.factor(scores_long$Score)
+
+# Run mixed model: Area as fixed, program as random; 
+# setting threshold = "equidistant" avoids downstream errors in post-hoc tests
+scores_clmm <- ordinal::clmm(ScoreOrdinal ~ Area + (1|Program),
+                             data = scores_long,
+                             threshold = "equidistant") 
+
+emm <- emmeans(scores_clmm, ~ Area)
+pairs(emm, adjust = "tukey")

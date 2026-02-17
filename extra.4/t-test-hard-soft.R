c374ef3e2c406ef6e8386aa0c88a5e238cb3f777
Torbet McNeil <tmcneil@arizona.edu>
2026-02-17 12:36:59 -0700

    Update t-test-hard-soft.R

--- extra.4/t-test-hard-soft.R
+++ extra.4/t-test-hard-soft.R
@@ -1,6 +1,7 @@
 # read in data
-nasem_all <- read.csv("C:/Users/ual-laptop/Desktop/t_test.2_file.csv")
+nasem_all <- read.csv(file = "data/t_test.2_file.csv")
 
 # t-test
 # use var.equal=TRUE for equal variances
+
 t.test(nasem_all$Area.Subarea_Score_Technical, nasem_all$Area.Subarea_Score_Soft, var.equal = TRUE) 

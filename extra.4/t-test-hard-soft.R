4ed238506c4024a91e053022d0f5d3ad166e8a90
Torbet McNeil <tmcneil@arizona.edu>
2026-02-20 21:34:50 -0700

    Update and rename t-test-hard-soft.R to t-test-technical-soft.R

--- extra.4/t-test-hard-soft.R
+++
@@ -1,7 +0,0 @@
-# read in data
-nasem_all <- read.csv(file = "data/t_test.2_file.csv")
-
-# t-test
-# use var.equal=TRUE for equal variances
-
-t.test(nasem_all$Area.Subarea_Score_Technical, nasem_all$Area.Subarea_Score_Soft, var.equal = TRUE) 

0dc966cfbc6df2679659758ed7e1381a37976cd4
Torbet McNeil <tmcneil@arizona.edu>
2026-02-20 21:19:49 -0700

    Update ordinal-data-modeling.R

--- mixed-models/ordinal-data-modeling.R
+++ mixed-models/ordinal-data-modeling.R
@@ -1,10 +1,12 @@
-install.packages("ordinal")
+# Ordinal mixed-effects model for "Data modeling and assessmentn" area and 
+# post-hoc pairwise comparisons to assess significant differences between sub-areas
+
 library(ordinal)
-install.packages("emmeans")
 library(emmeans)
 
 # Read data
 scores_long <- read.csv(file = "data/data_modeling_long.csv")
+
 # Ordinal package is expecting a factor for the response variable
 scores_long$ScoreOrdinal <- as.factor(scores_long$Score)
 
@@ -14,6 +16,6 @@ scores_clmm <- ordinal::clmm(ScoreOrdinal ~ Area + (1|Program),
                              data = scores_long,
                              threshold = "equidistant") 
 
+# Run post-hoc pairwise comparisons
 emm <- emmeans(scores_clmm, ~ Area)
-
 pairs(emm, adjust = "tukey")

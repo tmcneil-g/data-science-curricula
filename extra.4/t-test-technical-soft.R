67750a221f2a40e5e15880b07099a994717bae78
Torbet McNeil <tmcneil@arizona.edu>
2026-02-26 19:54:24 -0700

    Update t-test-technical-soft.R

--- extra.4/t-test-technical-soft.R
+++ extra.4/t-test-technical-soft.R
@@ -2,8 +2,8 @@
 # Areas/sub-areas categorized as “technical” scored higher than areas/sub-areas categorized as “soft” or “soft/technical.”
 
 # Read data
-nasem_all <- read.csv(file = "data/t_test.2_file.csv")
+nasem_all <- read.csv(file = "data/technical-soft-skills.csv")
 
 # Run one-tailed Student's t-test
 # Use var.equal=TRUE for equal variances
-t.test(nasem_all$Area.Subarea_Score_Technical, nasem_all$Area.Subarea_Score_Soft, var.equal = TRUE) 
+t.test(nasem_all$Area_Subarea_Score_Technical, nasem_all$Area_Subarea_Score_Soft, var.equal = TRUE) 
